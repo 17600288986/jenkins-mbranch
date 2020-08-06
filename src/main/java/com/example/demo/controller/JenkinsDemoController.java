4cc68c386f3fa06f6104f0713d0b90eedb82c3ed
gaoxw <Gaoxw931067278@163.com>
2020-08-06 17:04:47 +0800

    jenkins-mbranch

---
+++ src/main/java/com/example/demo/controller/JenkinsDemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/jenkins")
+public class JenkinsDemoController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String jenkinsTest(){
+
+        return "Hello,Jenkins";
+    }
+}
